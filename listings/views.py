980f1a0fafaf2740f43212039ad8a06491995f8e
Joel Bebo <joelbebo@gmail.com>
2019-06-08 21:43:46 -0400

    link lingsting.html to database

--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
+from .models import Listing
+listings = Listing.objects.all()
+
+
+context = {
+    'listings': listings
+}
+
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    return render(request, 'listings/listings.html', context)
 
 def listing(request):
     return render(request, 'listings/listing.html')
