2a2b20ade732082b2597a25c43fdcbf770a9ae9f
Joel Bebo <joelbebo@gmail.com>
2019-06-15 18:48:38 -0400

    Dashboard functioality

--- btre/settings.py
+++ btre/settings.py
@@ -141,3 +141,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'joelbebo@gmail.com'
+EMAIL_HOST_PASSWORD = '' #my gmail password here so an auto email can be generated from an inquiry submission.
+EMAIL_USE_TLS=True
